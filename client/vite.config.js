ff3ad707e315af8b14738d74679950c3cc276840
higoreduardodocs <higoreduardodocs@gmail.com>
2023-09-16 14:56:28 -0300

    :art: Feat: Create build local

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +14,12 @@ export default defineConfig({
     host: true,
     strictPort: true,
     port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
   },
 })
